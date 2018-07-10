54baab9f446a8c6cf9950e7e484abc35461fb920
Carlos Londoño <caalondono@bancolombia.com.co>
2018-07-10 16:38:54 -0500

    First reactive route and component

---
+++ src/main/java/com/projects/webfluxcrud/routers/ClientRouter.java
@@ -0,0 +1,22 @@
+package com.projects.webfluxcrud.routers;
+
+import com.projects.webfluxcrud.handlers.ClientHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class ClientRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(ClientHandler clientHandler) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/clients")
+                        .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+                        clientHandler::getAllClients);
+    }
+}
